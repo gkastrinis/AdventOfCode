bd6e9b1b614e2809da2f6eb15ae8d474c3347093
George Kastrinis <george.kast@gmail.com>
2024-12-02 19:07:19 +0200

    Add the second part of Day1

--- Day1/aoc_24_1.jl
+++ Day1/aoc_24_1.jl
@@ -26,18 +26,30 @@ function prepare_input(input::String)
         list1[i] = parse(Int, parts[1])
         list2[i] = parse(Int, parts[2])
     end
+    sort!(list1)
+    sort!(list2)
     return (list1, list2)
 end
 
 function solve(input::String)
     list1, list2 = prepare_input(input)
-    sort!(list1)
-    sort!(list2)
+    return (solve_part1(list1, list2), solve_part2(list1, list2))
+end
+
+function solve_part1(list1::Vector{Int}, list2::Vector{Int})
     diffs = Vector{Int}(undef, length(list1))
     for i in 1:length(list1)
         diffs[i] = abs(list1[i] - list2[i])
     end
     return sum(diffs)
+end
+
+function solve_part2(list1::Vector{Int}, list2::Vector{Int})
+    score = 0
+    for num in list1
+        score += num * count(==(num), list2)
+    end
+    return score
 end
 
 end
