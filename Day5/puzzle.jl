b4fa3e4cc2393c5ad95141a12ea9fe78f85338fc
George Kastrinis <george.kast@gmail.com>
2024-12-05 09:51:50 +0200

    feat: add solution for Day5/Part1

--- Day5/puzzle.jl
+++ Day5/puzzle.jl
@@ -1,32 +1,65 @@
 module AoC_24_Day5
 
-run_actual(path::String) = solve(read(path, String))
+run(path::String) = solve(read(path, String))
 
-function run_example()
-    input = """
-..
-"""
-    return solve(input)
+Base.@kwdef struct Manual
+    IsBefore::Dict{Int, Set{Int}} = Dict{Int, Set{Int}}()
+    IsAfter::Dict{Int, Set{Int}} = Dict{Int, Set{Int}}()
+    Updates::Vector{Vector{Int}} = Vector{Vector{Int}}()
 end
 
 function prepare_input(input::String)
-    return nothing
+    manual = Manual()
+    io = IOBuffer(input)
+    while !eof(io)
+        line = readline(io)
+        # We are in the second part of the input. Stop this loop.
+        isempty(line) && break
+
+        delim = findfirst(==('|'), line)
+        x = parse(Int, @view line[1:delim-1])
+        y = parse(Int, @view line[delim+1:end])
+        manual.IsBefore[x] = get!(manual.IsBefore, x, Set{Int}()) ∪ Set([y])
+        manual.IsAfter[y] = get!(manual.IsAfter, y, Set{Int}()) ∪ Set([x])
+    end
+    while !eof(io)
+        line = readline(io)
+        isempty(line) && break
+        pages = split(line, ",")
+        push!(manual.Updates, parse.(Int, pages))
+    end
+    return manual
 end
 
 function solve(input::String)
-    x = prepare_input(input)
-    printstyled("Part 1: ", part1(x), "\n"; color=:yellow)
-    printstyled("Part 2: ", part2(x), "\n"; color=:blue)
+    manual = prepare_input(input)
+    printstyled("Part 1: ", part1(manual), "\n"; color=:yellow)
+    printstyled("Part 2: ", part2(manual), "\n"; color=:blue)
     return nothing
 end
 
-#
-function part1(x)
-    return nothing
+function is_before_the_rest(manual::Manual, page::Int, rest::AbstractVector{Int})
+    return all(
+        after -> haskey(manual.IsBefore, page) && after in manual.IsBefore[page],
+        rest
+    )
+end
+
+# 4959
+function part1(manual::Manual)
+    score = 0
+    for update in manual.Updates
+        is_ok = true
+        for (i, page) in enumerate(update)
+            is_ok = is_ok && is_before_the_rest(manual, page, @view update[i+1:end])
+        end
+        is_ok && (score += update[length(update)÷2 + 1])
+    end
+    return score
 end
 
 #
-function part2(x)
+function part2(manual::Manual)
     return nothing
 end
 
