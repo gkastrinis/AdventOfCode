d9dc7a31523756d0f71f421cd18924965c83919b
George Kastrinis <george.kast@gmail.com>
2024-12-05 14:33:58 +0200

    refactor: rename a couple fields in Day5

--- Day5/puzzle.jl
+++ Day5/puzzle.jl
@@ -3,8 +3,8 @@ module AoC_24_Day5
 run(path::String) = solve(read(path, String))
 
 Base.@kwdef struct Manual
-    IsBefore::Dict{Int, Set{Int}} = Dict{Int, Set{Int}}()
-    Updates::Vector{Vector{Int}} = Vector{Vector{Int}}()
+    PagePrecedsPages::Dict{Int, Set{Int}} = Dict{Int, Set{Int}}()
+    Pages::Vector{Vector{Int}} = Vector{Vector{Int}}()
 end
 
 function prepare_input(input::String)
@@ -18,13 +18,13 @@ function prepare_input(input::String)
         delim = findfirst(==('|'), line)
         x = parse(Int, @view line[1:delim-1])
         y = parse(Int, @view line[delim+1:end])
-        manual.IsBefore[x] = get!(manual.IsBefore, x, Set{Int}()) ∪ Set([y])
+        manual.PagePrecedsPages[x] = get!(manual.PagePrecedsPages, x, Set{Int}()) ∪ Set([y])
     end
     while !eof(io)
         line = readline(io)
         isempty(line) && break
         pages = split(line, ",")
-        push!(manual.Updates, parse.(Int, pages))
+        push!(manual.Pages, parse.(Int, pages))
     end
     return manual
 end
@@ -37,7 +37,7 @@ function solve(input::String)
 end
 
 function is_before(manual::Manual, page::Int, other::Int)
-    return haskey(manual.IsBefore, page) && other in manual.IsBefore[page]
+    return haskey(manual.PagePrecedsPages, page) && other in manual.PagePrecedsPages[page]
 end
 
 function middle_page(pages::AbstractVector{Int})
@@ -51,13 +51,13 @@ end
 
 # 4959
 function part1(manual::Manual)
-    return sum(middle_page(pages) for pages in manual.Updates if in_correct_order(manual, pages))
+    return sum(middle_page(pages) for pages in manual.Pages if in_correct_order(manual, pages))
 end
 
 # 4655
 function part2(manual::Manual)
     score = 0
-    for pages in manual.Updates
+    for pages in manual.Pages
         page_len = length(pages)
         changed = false
         for i in 1:page_len
