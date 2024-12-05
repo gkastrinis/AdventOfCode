e20792e018e826e9de3730fc8ca1fb31a6a5acb5
George Kastrinis <george.kast@gmail.com>
2024-12-05 12:26:24 +0200

    chore: add puzzle template file

---
+++ puzzle_template.jl
@@ -0,0 +1,26 @@
+module AoC_24_DayX
+
+run(path::String) = solve(read(path, String))
+
+function preprocess(input::String)
+    return nothing
+end
+
+function solve(input::String)
+    x = preprocess(input)
+    printstyled("Part 1: ", part1(x), "\n"; color=:yellow)
+    printstyled("Part 2: ", part2(x), "\n"; color=:blue)
+    return nothing
+end
+
+#
+function part1(x)
+    return nothing
+end
+
+#
+function part2(x)
+    return nothing
+end
+
+end
