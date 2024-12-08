2d779d78ee030eb07eac4fc0f0c29c608a714a4b
George Kastrinis <george.kast@gmail.com>
2024-12-08 13:31:39 +0200

    chore: minor update in the puzzle template

--- puzzle_template.jl
+++ puzzle_template.jl
@@ -7,17 +7,13 @@ function Input(input::String)
     return Input()
 end
 
-function clone(input::Input)
-    return Input()
-end
-
 solve_file(path::String) = solve(read(path, String))
 
 function solve(inputStr::String)
     input = Input(inputStr)
-    input_clone = clone(input)
-    printstyled("Part 1: ", Part1.solve(input), "\n"; color=:yellow)
-    printstyled("Part 2: ", Part2.solve(input_clone), "\n"; color=:blue)
+    input_clone = deepcopy(input)
+    printstyled("Part 1: ", Part1.solve(input), "\n"; color=:blue)
+    printstyled("Part 2: ", Part2.solve(input_clone), "\n"; color=:yellow)
     return nothing
 end
 
