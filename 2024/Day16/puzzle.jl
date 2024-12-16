fec8131f27a4191dd675d42dad4c791139206eb8
George Kastrinis <george.kast@gmail.com>
2024-12-17 00:44:03 +0200

    chore: simplify code in Day16

--- 2024/Day16/puzzle.jl
+++ 2024/Day16/puzzle.jl
@@ -42,13 +42,6 @@ end
 
 using .AoC_Utils: Direction, Point, N, S, E, W
 
-mutable struct GridNode
-    score::Int
-    out1::Union{GridNode,Nothing}
-    out2::Union{GridNode,Nothing}
-    turn::Union{GridNode,Nothing}
-end
-
 function has_to_turn(tag::Char, direction::Direction)
     return (tag == 'H' && direction in (N, S)) || (tag == 'V' && direction in (E, W))
 end
@@ -59,88 +52,51 @@ end
 
 const TaggedPoint = Tuple{Point,Char}
 
-# Two nodes corresponding to the horizontal and vertical traversal of the grid cell.
-# Given a grid like this:
-# . X .
-# A G B
-# . Y .
-# The following represents cell G
-#  +------|----|----+
-#  |      |    |    |
-#  |      |  +---+  |
-#  |   +--+  | V |  |
-#  |   |     +---+  |
-#  | +-|-+     |    |
-# ---| H |-------------
-#  | +---+     |    |
-#  |           |    |
-#  +-----------|----+
-#              |
-#
-# A is connected to B via H, and X is connected to Y via V.
-# A is connected to X via the turn edge in H, and B is connected to Y via the turn edge in V.
-# Thus given a node, the out edges have score 1, the turn edges have score 1001.
-function create_graph(puzzle::Puzzle)
-    nodes = Dict{TaggedPoint, GridNode}()
+function initialize_distances(puzzle::Puzzle)
+    distances = Dict{TaggedPoint,Int}()
     rows, columns = size(puzzle.grid)
     # Inialize scores in each node to a large number (simulating infinity)
     max_score = rows * columns * 1000 * 1000
     # Ignore the outer walls
     for i in 2:rows-1
         for j in 2:columns-1
-            pos = (i, j)
             # Ignore inner walls
             puzzle.grid[i, j] == '#' && continue
 
-            # For the current position, create one node for horizontal and one for vertical
-            # traversal. Also, link the nodes with their appropriate "previous" nodes
-            # Horizontal traversal:
-            left = pos + W
-            prev = get(nodes, (left, 'H'), nothing)
-            nodeH = GridNode(max_score, prev, nothing, nothing)
-            isnothing(prev) || (prev.out2 = nodeH)
-            # Vertical traversal:
-            top = pos + N
-            prev = get(nodes, (top, 'V'), nothing)
-            nodeV = GridNode(max_score, prev, nothing, nothing)
-            isnothing(prev) || (prev.out2 = nodeV)
-            # Link the nodes with their appropriate "turn" nodes
-            nodeH.turn = nodeV
-            nodeV.turn = nodeH
-
-            nodes[pos, 'H'] = nodeH
-            nodes[pos, 'V'] = nodeV
-
-            # The source node has a score of 0
-            pos == puzzle.start && (nodeH.score = 0)
+            pos = (i, j)
+            distances[pos, 'V'] = max_score
+            # The source node has a score of 0, for horizontal traversals
+            distances[pos, 'H'] = (pos == puzzle.start ? 0 : max_score)
         end
     end
-    return nodes
+    return distances
 end
 
-function shortest_path!(puzzle::Puzzle, nodes::Dict{TaggedPoint,GridNode})
+function shortest_path!(puzzle::Puzzle, distances::Dict{TaggedPoint,Int})
     visited = Set{Point}()
-    unvisited = map(k -> (k[1], k[2], nodes[k]), collect(keys(nodes)))
     prev = Dict{TaggedPoint,TaggedPoint}()
     while true
-        isempty(unvisited) && break
-        sort!(unvisited; by = node -> node[3].score, rev = true)
+        candidates = filter(tagged_point -> !(tagged_point[1] in visited), collect(keys(distances)))
+        isempty(candidates) && break
+        # (position x tag x distance)
+        unvisited = map(k -> (k[1], k[2], distances[k]), candidates)
+        # Sort by increasing distance
+        sort!(unvisited; by = node -> node[3], rev = true)
 
-        pos, tag, node = pop!(unvisited)
+        pos, tag, distance = pop!(unvisited)
         pos == puzzle.stop && break
         push!(visited, pos)
 
         for dir in (N, S, E, W)
             neighbor = pos + dir
             neighbor in visited && continue
-            neighbor_tag = dir_to_tag(dir)
-            neighbor_node = get(nodes, (neighbor, neighbor_tag), nothing)
-            isnothing(neighbor_node) && continue
-
-            new_score = node.score + (has_to_turn(tag, dir) ? 1001 : 1)
-            if new_score < neighbor_node.score
-                neighbor_node.score = new_score
-                prev[neighbor, neighbor_tag] = (pos, tag)
+            tagged_neighbor = (neighbor, dir_to_tag(dir))
+            neighbor_distance = get(distances, tagged_neighbor, nothing)
+            isnothing(neighbor_distance) && continue
+            new_distance = distance + (has_to_turn(tag, dir) ? 1001 : 1)
+            if new_distance < neighbor_distance
+                distances[tagged_neighbor] = new_distance
+                prev[tagged_neighbor] = (pos, tag)
             end
         end
     end
@@ -150,57 +106,55 @@ end
 ############################################################################################
 
 module Part1
-    using ..AoC_24_Day16: Puzzle, create_graph, shortest_path!
+    using ..AoC_24_Day16: Puzzle, initialize_distances, shortest_path!, pretty_print
 
     function solve(puzzle::Puzzle)
-        nodes = create_graph(puzzle)
-        shortest_path!(puzzle, nodes)
-        return min(nodes[puzzle.stop, 'H'].score, nodes[puzzle.stop, 'V'].score)
+        distances = initialize_distances(puzzle)
+        shortest_path!(puzzle, distances)
+        return min(distances[puzzle.stop, 'H'], distances[puzzle.stop, 'V'])
     end
 end
 
 ############################################################################################
 
 module Part2
-    using ..AoC_24_Day16: Puzzle, TaggedPoint, create_graph, shortest_path!
+    using ..AoC_24_Day16: Puzzle, TaggedPoint, initialize_distances, shortest_path!
 
     function solve(puzzle::Puzzle)
-        nodes = create_graph(puzzle)
-        prev = shortest_path!(puzzle, nodes)
-
-        stopH = nodes[puzzle.stop, 'H']
-        stopV = nodes[puzzle.stop, 'V']
-        min_score = min(stopH.score, stopV.score)
-        min_tag = stopH.score == min_score ? 'H' : 'V'
-
-        working_set = Set{TaggedPoint}()
-        if stopH.score == min_score
-            push!(working_set, (puzzle.stop, 'H'))
-        end
-        if stopV.score == min_score
-            push!(working_set, (puzzle.stop, 'V'))
-        end
+        distances = initialize_distances(puzzle)
+        prev = shortest_path!(puzzle, distances)
 
         # Use prev from Dijkstra's algorithm to find the shortest path
         # Extend with alternative turns to discover all shortest paths
+
+        stop_distance_h = distances[puzzle.stop, 'H']
+        stop_distance_v = distances[puzzle.stop, 'V']
+        min_score = min(stop_distance_h, stop_distance_v)
+
+        working_set = Set{TaggedPoint}()
+        if stop_distance_h == min_score
+            push!(working_set, (puzzle.stop, 'H'))
+        end
+        if stop_distance_v == min_score
+            push!(working_set, (puzzle.stop, 'V'))
+        end
         while !isempty(working_set)
             curr_pos, curr_tag = pop!(working_set)
             puzzle.grid[curr_pos...] = '*'
 
-            haskey(prev, (curr_pos, curr_tag)) || continue
-            p, tag = prev[curr_pos, curr_tag]
-            puzzle.grid[p...] = '*'
-            push!(working_set, (p, tag))
+            prev_info = get(prev, (curr_pos, curr_tag), nothing)
+            isnothing(prev_info) && continue
+            prev_pos, prev_tag = prev_info
+            push!(working_set, (prev_pos, prev_tag))
 
             # If the previous node is on a turn, check if the node straight ahead of current
             # is also valid based on the score.
-            if curr_tag != tag
-                diff = p - curr_pos
-                alt = curr_pos + diff + diff
-                alt_node = get(nodes, (alt, curr_tag), nothing)
-                isnothing(alt_node) && continue
-                alt_node.score != nodes[curr_pos, curr_tag].score - 2 && continue
-                push!(working_set, (alt, curr_tag))
+            if curr_tag != prev_tag
+                diff = prev_pos - curr_pos
+                alt_pos = curr_pos + diff + diff
+                alt_distance = get(distances, (alt_pos, curr_tag), nothing)
+                alt_distance != distances[curr_pos, curr_tag] - 2 && continue
+                push!(working_set, (alt_pos, curr_tag))
             end
         end
         return count(==('*'), puzzle.grid)
