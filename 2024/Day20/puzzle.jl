bfbe1f337868e9575807ef98b6c2981d3fc66053
George Kastrinis <george.kast@gmail.com>
2024-12-20 16:15:33 +0200

    chore: update methods for Day20

--- 2024/Day20/puzzle.jl
+++ 2024/Day20/puzzle.jl
@@ -2,7 +2,8 @@ module AoC_24_Day20
 
 include("../../AoC_Utils.jl")
 import .AoC_Utils: pretty_print
-using .AoC_Utils: Point, read_grid
+using .AoC_Utils: Point, N, S, E, W
+using .AoC_Utils: read_grid, in_bounds
 
 struct Puzzle
     grid::Matrix{Char}
@@ -23,75 +24,6 @@ function Puzzle(input::String)
     return Puzzle(grid, start, stop)
 end
 
-############################################################################################
-
-using .AoC_Utils: N, S, E, W
-using DataStructures: MutableBinaryMinHeap, update!
-
-function Base.isless(p1::Tuple{Point, Int}, p2::Tuple{Point, Int})
-    return p1[2] < p2[2] || (p1[2] == p2[2] && p1[1] < p2[1])
-end
-
-function shortest_path!(grid::Matrix{Char}, start::Point, stop::Point)
-    rows, columns = size(grid)
-    # Inialize scores in each node to a large number (simulating infinity)
-    max_score = rows * columns * 1000
-
-    # Use a heap to store nodes and distances, to retrieve the min node each time
-    # Use a dict from node to heap handle to update the heap
-    # Use a dict from node to current min distance
-    unvisited = MutableBinaryMinHeap{Tuple{Point, Int}}()
-    handles = Dict{Point,Int}()
-
-    for i in 1:rows, j in 1:columns
-        grid[i, j] == '#' && continue
-        pos = (i, j)
-        dist = (pos == start ? 0 : max_score)
-        h = push!(unvisited, (pos, dist))
-        handles[pos] = h
-    end
-
-    visited = Set{Point}()
-    previous = Dict{Point,Point}()
-    path_score = max_score
-    while true
-        pos, distance = pop!(unvisited)
-        if pos == stop
-            path_score = distance
-            break
-        end
-        push!(visited, pos)
-
-        for dir in (N, S, E, W)
-            neighbor = pos + dir
-            neighbor in visited && continue
-            neighbor_handle = get(handles, neighbor, nothing)
-            isnothing(neighbor_handle) && continue
-            new_distance = distance + 1
-            if new_distance < unvisited[neighbor_handle][2]
-                update!(unvisited, neighbor_handle, (neighbor, new_distance))
-                previous[neighbor] = pos
-            end
-        end
-    end
-
-    path = get_path(previous, stop)
-    return (path_score != max_score, path, path_score, previous)
-end
-
-function get_path(previous::Dict{Point,Point}, stop::Point)
-    path = Vector{Point}()
-    working_set = Set{Point}([stop])
-    while !isempty(working_set)
-        current = pop!(working_set)
-        push!(path, current)
-        prev = get(previous, current, nothing)
-        isnothing(prev) && continue
-        push!(working_set, prev)
-    end
-    return path
-end
-
 const TO_COLOR = Dict(
     'S' => :blue,
     'E' => :green,
@@ -109,68 +41,99 @@ function AoC_Utils.pretty_print(puzzle::Puzzle)
     end
 end
 
+function collect_path(grid::Matrix{Char}, start::Point, stop::Point)
+    path = Vector{Point}()
+    path_indexes = Dict{Point,Int}()
+    current, i = start, 0
+    while current != stop
+        push!(path, current)
+        path_indexes[current] = i
+        i += 1
+        for dir in (N, S, E, W)
+            next = current + dir
+            in_bounds(grid, next) || continue
+            grid[next...] != '#' || continue
+            !haskey(path_indexes, next) || continue
+            current = next
+            break
+        end
+    end
+    push!(path, stop)
+    path_indexes[stop] = i
+    return path, path_indexes
+end
+
+mutable struct CheatInfo
+    grid::Matrix{Char}
+    path::Vector{Point}
+    path_indexes::Dict{Point,Int}
+    threshold::Int
+    cheats::Set{Tuple{Point,Point}}
+end
+
+function attempt_to_cheat(ci::CheatInfo, origin::Point, n_cheats::Int)
+    for dir in (N, S, E, W)
+        neighbor = origin + dir
+        in_bounds(ci.grid, neighbor) || continue
+        ci.grid[neighbor...] == '#' || continue
+        # Only start cheating if at a wall
+        cheat(ci, origin, neighbor, n_cheats - 1)
+    end
+end
+
+function cheat(ci::CheatInfo, origin::Point, position::Point, n_cheats::Int)
+    # printstyled(origin, " -> ", position, " ", n_cheats, "\n"; color=:red)
+    if n_cheats == 0
+        # Shouldn't end on a wall
+        ci.grid[position...] == '#' && return
+        # Shouldn't end on the origin
+        position == origin && return
+        # Should end at a point already in the path
+        haskey(ci.path_indexes, position) || return
+        # Cheating should only improve the path
+        saves = ci.path_indexes[origin] - ci.path_indexes[position] - 2
+        saves >= ci.threshold || return
+        # Avoid duplicates
+        pair = (origin > position ? (origin, position) : (position, origin))
+        # printstyled(pair; color=:red)
+        push!(ci.cheats, pair)
+        return
+    end
+
+    for dir1 in (N, S, E, W)
+        neighbor = position + dir1
+        in_bounds(ci.grid, neighbor) || continue
+        cheat(ci, origin, neighbor, n_cheats - 1)
+    end
+end
+
+function debug_cheats(puzzle::Puzzle, cheats::Set{Tuple{Point,Point}})
+    for pair in cheats
+        a, b = puzzle.grid[pair[1]...], puzzle.grid[pair[2]...]
+        puzzle.grid[pair[1]...] = 'X'
+        puzzle.grid[pair[2]...] = 'X'
+        pretty_print(puzzle)
+        puzzle.grid[pair[1]...] = a
+        puzzle.grid[pair[2]...] = b
+    end
+end
+
 ############################################################################################
 
 module Part1
-    using ..AoC_Utils: Point, N, S, E, W, in_bounds
-    using ..AoC_24_Day20: Puzzle, pretty_print, shortest_path!
+    using ..AoC_Utils: Point
+    using ..AoC_24_Day20: Puzzle, CheatInfo
+    using ..AoC_24_Day20: pretty_print, collect_path, attempt_to_cheat, debug_cheats
 
-    function solve(puzzle::Puzzle, threshold::Int)
+    function solve(puzzle::Puzzle, n_cheats::Int, threshold::Int)
+        path, path_indexes = collect_path(puzzle.grid, puzzle.start, puzzle.stop)
         # pretty_print(puzzle)
-        _, path, path_score, _ = shortest_path!(puzzle.grid, puzzle.start, puzzle.stop)
-
-        path_set = Set(path)
-        cheat_pairs = Set{Tuple{Point,Point}}()
+        ci = CheatInfo(puzzle.grid, path, path_indexes, threshold, Set{Tuple{Point,Point}}())
         for p in path
-            for dir1 in (N, S, E, W)
-                neighbor1 = p + dir1
-                in_bounds(puzzle.grid, neighbor1) || continue
-                puzzle.grid[neighbor1...] == '#' || continue
-
-                for dir2 in (N, S, E, W)
-                    neighbor2 = neighbor1 + dir2
-                    in_bounds(puzzle.grid, neighbor2) || continue
-                    puzzle.grid[neighbor2...] != '#' || continue
-                    neighbor2 != p || continue
-                    neighbor2 in path_set || continue
-
-                    k = findfirst(==(p), path)
-                    l = findfirst(==(neighbor2), path)
-
-                    saves = k - l - 2
-                    saves > 0 || continue
-                    saves >= threshold || continue
-
-
-                    # l > k && continue
-                    # k - l <= 2 && continue
-                    # k - l >= threshold || continue
-
-                    # println("checking cheating $p with $neighbor1 to $neighbor2 -- $saves")
-
-                    pair = (neighbor1 > neighbor2 ? (neighbor1, neighbor2) : (neighbor2, neighbor1))
-                    push!(cheat_pairs, pair)
-                end
-            end
+            attempt_to_cheat(ci, p, n_cheats)
         end
-        i = 0
-        # for pair in cheat_pairs
-        #     i += 1
-        #     a, b = puzzle.grid[pair[1]...], puzzle.grid[pair[2]...]
-        #     puzzle.grid[pair[1]...] = 'X'
-        #     puzzle.grid[pair[2]...] = 'X'
-        #     pretty_print(puzzle)
-        #     puzzle.grid[pair[1]...] = a
-        #     puzzle.grid[pair[2]...] = b
-        #     i > 10 && break
-        # end
-        # println(length(cheat_pairs))
-        # println(cheat_pairs)
-        # pretty_print(puzzle)
-        # println(path_score)
-        # println(length(path))
-        # println(path)
-        return length(cheat_pairs)
+        # debug_cheats(puzzle, ci.cheats)
+        return length(ci.cheats)
     end
 end
 
@@ -188,21 +151,22 @@ end
 
 using .AoC_Utils: @filedata, test_assert
 
-solve_part1(path::String, threshold::Int) = Part1.solve(Puzzle(@filedata path), threshold)
+solve_part1(path::String, n_cheats::Int, threshold::Int) =
+    Part1.solve(Puzzle(@filedata path), n_cheats, threshold)
 solve_part2(path::String) = Part2.solve(Puzzle(@filedata path))
 
 function test()
     for (path, args) in [
-        ("example1.txt" => ((44, 2), nothing)),
-        ("example1.txt" => ((2, 40), nothing)),
-        ("example1.txt" => ((1, 64), nothing)),
-        ("example1.txt" => ((0, 65), nothing)),
+        ("example1.txt" => ((2, 2, 44), nothing)),
+        ("example1.txt" => ((2, 40, 2), nothing)),
+        ("example1.txt" => ((2, 64, 1), nothing)),
+        ("example1.txt" => ((2, 65, 0), nothing)),
     ]
         args1, args2 = args
-        expected1, threshold = args1
+        n_cheats, threshold, expected1= args1
         expected2 = args2
         printstyled("--- testing: ", path, " ---\n"; color=:yellow)
-        test_assert("Part 1", expected1, solve_part1(path, threshold))
+        test_assert("Part 1", expected1, solve_part1(path, n_cheats, threshold))
         test_assert("Part 2", expected2, solve_part2(path))
     end
     return nothing
