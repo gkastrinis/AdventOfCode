03c7ef0607bfa182c961b81922a7f82ad6ee4356
George Kastrinis <george.kast@gmail.com>
2024-12-22 00:48:42 +0200

    feat: add solution for Day21/Part2

--- 2024/Day21/puzzle.jl
+++ 2024/Day21/puzzle.jl
@@ -1,6 +1,7 @@
 module AoC_24_Day21
 
 include("../../AoC_Utils.jl")
+using .AoC_Utils: @n_times, Point
 
 struct Puzzle
     codes::Vector{String}
@@ -32,59 +33,148 @@ const ROBOT_BUTTONS = Dict(
     '>' => (2, 3),
 )
 
+function press_button(io::IO, from::Point, to::Point, buttons::Dict{Char, Point}, empty_btn::Point)
+    h = to[2] - from[2]
+    v = to[1] - from[1]
+    h_ch = h < 0 ? '<' : '>'
+    v_ch = v < 0 ? '^' : 'v'
+
+    if (h_ch == '<' && from + (0, h) != empty_btn) || from + (v, 0) == empty_btn
+        @n_times abs(h) print(io, h_ch)
+        @n_times abs(v) print(io, v_ch)
+    else
+        @n_times abs(v) print(io, v_ch)
+        @n_times abs(h) print(io, h_ch)
+    end
+    print(io, 'A')
+    return nothing
+end
+
 ############################################################################################
 
 module Part1
-    using ..AoC_Utils: @n_times, Point, manhattan_distance
-    using ..AoC_24_Day21: Puzzle, DOOR_BUTTONS, ROBOT_BUTTONS
+    using ..AoC_Utils: Point
+    using ..AoC_24_Day21: Puzzle, DOOR_BUTTONS, ROBOT_BUTTONS, press_button
 
     function solve(puzzle::Puzzle)
         return sum(press_code(code) for code in puzzle.codes)
     end
 
     function press_code(code::String)
-        seq1 = press_buttons(code, DOOR_BUTTONS)
-        seq2 = press_buttons(seq1, ROBOT_BUTTONS)
-        seq3 = press_buttons(seq2, ROBOT_BUTTONS)
+        seq1 = press_buttons(code, DOOR_BUTTONS, (4, 1))
+        seq2 = press_buttons(seq1, ROBOT_BUTTONS, (1, 1))
+        seq3 = press_buttons(seq2, ROBOT_BUTTONS, (1, 1))
         return parse(Int, (@view code[1:end-1])) * length(seq3)
     end
 
-    function press_buttons(code::String, buttons::Dict{Char, Point})
+    function press_buttons(code::String, buttons::Dict{Char, Point}, empty_btn::Point)
         io = IOBuffer()
         from = buttons['A']
         for button in code
             to = buttons[button]
-            press_button(io, from, to, buttons)
+            press_button(io, from, to, buttons, empty_btn)
             from = to
         end
         return String(take!(io))
-    end
-
-    function press_button(io::IO, from::Point, to::Point, buttons::Dict{Char, Point})
-        h = to[2] - from[2]
-        v = to[1] - from[1]
-        h_ch = h < 0 ? '<' : '>'
-        v_ch = v < 0 ? '^' : 'v'
-
-        if (h_ch == '<' && from + (0, h) != (4, 1)) || from + (v, 0) == (4, 1)
-            @n_times abs(h) print(io, h_ch)
-            @n_times abs(v) print(io, v_ch)
-        else
-            @n_times abs(v) print(io, v_ch)
-            @n_times abs(h) print(io, h_ch)
-        end
-        print(io, 'A')
-        return nothing
     end
 end
 
 ############################################################################################
 
 module Part2
-    using ..AoC_24_Day21: Puzzle
+    using ..AoC_Utils: Point
+    using ..AoC_24_Day21: Puzzle, DOOR_BUTTONS, ROBOT_BUTTONS, press_button
 
+    const ROBOT_BUTTONS_KEYS = ('A', '^', '<', 'v', '>')
+    const DOOR_BUTTONS_KEYS = ('7', '8', '9', '4', '5', '6', '1', '2', '3', '0', 'A')
+
+    # A human is pressing buttons on a robot (KeyPad1). This is free.
+    # Then the robot is pressing buttons on another robot (KeyPad2).
+    # If we want the first robot to go from "A" to "v" on KeyPad2, we have to press "v<A" on KeyPad1.
+    # For all possible button combinations we compute the best such sequence. This is "basic_sequences".
+    # We also compute the cost of each such sequence. In "v<A", the cost is 3.
+    # We compute the costs for all possible combinations of buttons in "costs1".
+    #
+    # Then we use dynamic programming to compute the costs for all possible button combinations
+    # from one keypad to the next. We always start and end at a "A".
+    # So for "v<A", we find the cost of "A to v" + "v to <" + "< to A".
+    #
+    # If we want to press "1A" on the numpad, with 25 robots in between,
+    # we compute "costs" 25 times in a row, and then use those costs for the sequence
+    # "^<<A>>vA" that corresponds to "A to 1" + "1 to A".
     function solve(puzzle::Puzzle)
-        return nothing
+        basic_sequences, costs1 = initial_costs(ROBOT_BUTTONS)
+        current = costs1
+        for _ in 1:24
+            current = next_costs(basic_sequences, current, ROBOT_BUTTONS)
+        end
+        last_costs = current
+
+        total_score = 0
+        for code in puzzle.codes
+            cost = 0
+            ch1 = 'A'
+            numpad_seq = get_numpad_sequence(code)
+            for ch2 in numpad_seq
+                cost += get(last_costs, (ch1, ch2), 1)
+                ch1 = ch2
+            end
+            total_score += parse(Int, (@view code[1:end-1])) * cost
+        end
+        return total_score
+    end
+
+    function initial_costs(buttons::Dict{Char, Point})
+        level = Dict{Tuple{Char, Char}, String}()
+        for btn1 in ROBOT_BUTTONS_KEYS, btn2 in ROBOT_BUTTONS_KEYS
+            btn1 == btn2 && continue
+            path = get_button_sequence(ROBOT_BUTTONS[btn1], ROBOT_BUTTONS[btn2], ROBOT_BUTTONS, (1, 1))
+            level[(btn1, btn2)] = path
+        end
+        costs = Dict{Tuple{Char, Char}, Int}()
+        for btn1 in ROBOT_BUTTONS_KEYS, btn2 in ROBOT_BUTTONS_KEYS
+            btn1 == btn2 && continue
+            len = length(level[(btn1, btn2)])
+            costs[(btn1, btn2)] = len
+        end
+        return (level, costs)
+    end
+
+    function next_costs(
+        basic_sequences::Dict{Tuple{Char, Char}, String},
+        prev_costs::Dict{Tuple{Char, Char}, Int},
+        buttons::Dict{Char, Point}
+    )
+        costs = Dict{Tuple{Char, Char}, Int}()
+        for btn1 in ROBOT_BUTTONS_KEYS, btn2 in ROBOT_BUTTONS_KEYS
+            btn1 == btn2 && continue
+            seq = basic_sequences[(btn1, btn2)]
+            ch1 = 'A'
+            cost = 0
+            for ch2 in seq
+                cost += get(prev_costs, (ch1, ch2), 1)
+                ch1 = ch2
+            end
+            costs[(btn1, btn2)] = cost
+        end
+        return costs
+    end
+
+    function get_button_sequence(from::Point, to::Point, buttons::Dict{Char, Point}, empty_btn::Point)
+        io = IOBuffer()
+        press_button(io, from, to, buttons, empty_btn)
+        return String(take!(io))
+    end
+
+    function get_numpad_sequence(code::String)
+        total_seq = ""
+        ch1 = 'A'
+        for ch2 in code
+            seq = get_button_sequence(DOOR_BUTTONS[ch1], DOOR_BUTTONS[ch2], DOOR_BUTTONS, (4, 1))
+            total_seq *= seq
+            ch1 = ch2
+        end
+        return total_seq
     end
 end
 
@@ -97,7 +187,7 @@ solve_part2(path::String) = Part2.solve(Puzzle(@filedata path))
 
 function test()
     for (path, args) in [
-        ("example1.txt" => (126384, nothing)),
+        ("example1.txt" => (126384, 154115708116294)),
     ]
         expected1, expected2 = args
         printstyled("--- testing: ", path, " ---\n"; color=:yellow)
